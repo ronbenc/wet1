4470a967e7037ae2bf154b5d67942d8271ae7bf2
Ron Benchetrit <ronbenc@campus.technion.ac.il>
2020-12-13 20:32:44 +0200

    Update RemoveCourse in CourseManager

--- CoursesManager.cpp
+++ CoursesManager.cpp
@@ -12,14 +12,20 @@ void CoursesManager::RemoveCourse(const int courseID)
   assert(courseID > 0);
   assert(course_map.count(courseID) != 0);
 
-  //CourseData to_remove(course_map[courseID]);
-  // for(int classID = 0; classID < to_remove.num_of_classes; classID++)
-  // {
-  //     if(to_remove.classes_array[classID] != nullptr)
-  //     {
-  //         to_remove.classes_array[classID]->erase(std::pair<int, int>(courseID, classID));
-  //     }
-  // }
+  CourseData to_remove(course_map[courseID]);
+  for(int classID = 0; classID < to_remove.num_of_classes; classID++)
+  {
+      if(to_remove.classes_array[classID] != most_viewed.end())
+      {
+        List_Map<int, ClassesTree>::iterator curr_time_it = to_remove.classes_array[classID];
+        curr_time_it->value.erase(std::pair<int, int>(courseID, classID));
+        
+        if(curr_time_it->value.empty())
+        {
+          most_viewed.erase(curr_time_it);
+        }
+      }
+  }
   course_map.erase(courseID);
 }
 
@@ -43,14 +49,14 @@ void CoursesManager::WatchClass(const int courseID, const int classID, int time)
   {
     assert(course_map[courseID].classes_array[classID] != most_viewed.end());
     curr_time_it->value.erase({courseID, classID});
-    // if(curr_time_it->value.empty())
-    // {
-    //   most_viewed.erase(curr_time_it);
-    // }
     int prev_time = curr_time_it->key;
     time += prev_time;
     most_viewed[time].insert(std::pair<int const, int const>(courseID, classID));
     course_map[courseID].classes_array[classID] = most_viewed.find_position(courseID); //optimize using hints! avoid double search
+    if(curr_time_it->value.empty())
+    {
+      most_viewed.erase(curr_time_it);
+    }
   }
 }
 
